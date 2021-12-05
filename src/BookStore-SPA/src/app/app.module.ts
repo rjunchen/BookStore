6b1e81ce90c0cb6d4cacd07c4e252372ec1eb902
rjunchen <rjunchen@gmail.com>
2021-12-05 14:18:09 -0700

    Setup up BookStore Components

--- src/BookStore-SPA/src/app/app.module.ts
+++ src/BookStore-SPA/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BookService } from './_services/book.service';
+import { CategoryService } from './_services/category.service';
+import { BookComponent } from './books/book/book.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { CategoryComponent } from './categories/category/category.component';
+import { CategoryListComponent } from './categories/category-list/category-list.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookListComponent,
+    CategoryComponent,
+    CategoryListComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [BookService, CategoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
